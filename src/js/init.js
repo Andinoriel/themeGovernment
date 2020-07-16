b27063a6fb557388e3f37d519e3502ffe19618b9
Nikolay Orzhekhovskii <icearrow256@gmail.com>
2020-07-16 18:29:39 +0300

    add jQuery plugins

---
+++ src/js/init.js
@@ -0,0 +1,9 @@
+import {appMakeBeCool} from "@js/app/lib/site";
+
+$(function(){
+    // if(global.cultureKey != 'en') {
+    //     downloadJSAtOnload(designUrl+'js/jquery/plugins/validation/localization/messages_'+global.cultureKey+'.js');
+    // }
+    "use strict";
+    appMakeBeCool.gateway.init();
+});
